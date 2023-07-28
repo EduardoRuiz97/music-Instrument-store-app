aa82192ea109793a02ea71cea7ca47fa04b0db97
Eduardo Ruiz <email.example@example.com>
2023-07-28 13:46:59 -0400

    detele of the tag head in the rootlayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,10 +22,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-        <head>
-          <link rel='icon' href='/favicon.ico' />
-        </head>
-
         <body>
           <ReduxProvider>
             {children}
